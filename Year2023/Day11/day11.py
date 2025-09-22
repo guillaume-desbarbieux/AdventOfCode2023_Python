2c19234ed6e768812a785986f901e028a9dbc9a2
guillaume-desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-09-22 19:27:13 +0200

    Day 11 part 1

--- Year2023/Day11/day11.py
+++ Year2023/Day11/day11.py
@@ -1,4 +1,5 @@
 from os import MFD_ALLOW_SEALING
+from pickletools import read_stringnl_noescape
 
 from Year2023.utils import read_file
 
@@ -9,38 +10,67 @@ def path(coord1, coord2) -> int:
     return abs(a - c) + abs(b - d)
 
 
-def expand(sky):
-    empty_lines = []
+def get_empty_rows_ans_cols(sky):
+    empty_rows = []
     empty_columns = []
     for i in range(len(sky)):
         if is_empty(sky[i]):
-            empty_lines.append(i)
+            empty_rows.append(i)
     for j in range(len(sky[0])):
-        empty = True
+        col_is_empty= True
         for i in range(len(sky)):
             if sky[i][j] == '#':
-                empty = False
+                col_is_empty = False
                 break
-        if empty:
+        if col_is_empty:
             empty_columns.append(j)
-    return empty_lines, empty_columns
+    return empty_rows, empty_columns
 
-def is_empty(line):
-    for i in line:
+def is_empty(row):
+    for i in row:
         if i == '#':
             return False
     return True
 
 
+def extend_rows(sky, rows):
+    extended_sky = []
+    for i in range(len(sky)-1, -1, -1):
+        extended_sky.append(sky[i])
+        if i in rows:
+            extended_sky.append(sky[i])
+    return extended_sky
+
+
+def extend_cols(sky, cols):
+    extended_sky = []
+    for i in range(len(sky)):
+        extended_sky.append([])
+
+    for j in range(len(sky[0])-1, -1, -1):
+        for i in range(len(sky)):
+            extended_sky[i].append(sky[i][j])
+            if j in cols:
+                extended_sky[i].append(sky[i][j])
+    return extended_sky
+
+
+def extend(sky, rows, cols):
+    rows_extended_sky = extend_rows(sky, rows)
+    return extend_cols(rows_extended_sky, cols)
+
+
+
+
 if __name__ == "__main__":
     sky = read_file("puzzle.txt")
-    a,b = expand(sky)
-    print(a,b)
+    rows,cols = get_empty_rows_ans_cols(sky)
+    extended_sky = extend(sky, rows, cols)
 
     galaxies = []
-    for i in range(len(sky)):
-        for j in range(len(sky[i])):
-            if sky[i][j] == '#':
+    for i in range(len(extended_sky)):
+        for j in range(len(extended_sky[i])):
+            if extended_sky[i][j] == '#':
                 galaxies.append((i, j))
 
     sum = 0
