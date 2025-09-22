64e5df008844f9a6e3e0d61292c9b87f09fdb5b7
guillaume-desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-09-22 23:00:58 +0200

    Day12 part1 en cours

--- Year2023/Day12/day12.py
+++ Year2023/Day12/day12.py
@@ -1,15 +1,18 @@
 from typing import Tuple, List
 
+import char
+
 from Year2023.utils import read_file, split
 
 def parse_puzzle(path:str):
-    rows: List[Tuple[str, List[int]]] = []
+    rows: List[Tuple[List[char], List[int]]] = []
     for line in read_file(path):
         parts = split(line, ' ')
         left = parts[0]
         right = parts[1]
+        springs = [c for c in left]
         nums = [int(x) for x in split(right, ',')]
-        rows.append((left, nums))
+        rows.append((springs, nums))
     return rows
 
 def get_right(springs:str):
@@ -30,8 +33,21 @@ def check_arrangement(row: Tuple[str,List[int]]):
     left, right = row
     return right == get_right(left)
 
+def list_arrangement(left):
+    # trouver l'index du permier ? et renvoyer les deux possibilités si trouvé
+    for i in range(len(left)):
+        if left[i] == '?':
+            left1 = left
+            left1[i] = '.'
+            left2 = left
+            left2[i] = '#'
+            return [left1,left2]
+    return left
+
 if __name__ == "__main__":
     rows = parse_puzzle("puzzle.txt")
+
+    print(list_arrangement([".", ".", "?", "."]))
 
     for row in rows:
         print(row)
